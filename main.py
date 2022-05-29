c22bbfd2cd4a1d6a482992dee716b9c83dac23cc
kwesiG <76437675+kwesiG@users.noreply.github.com>
2022-05-30 00:10:56 +0100

    Create screen constants and rgb values and set them within pygame

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ import pygame
 
 # Pygame's mixer and associated sounds effect music handling tool
 from pygame import mixer
+pygame.init()
+
+WIDTH = 1400
+HEIGHT = 800
+
+black = (0, 0, 0)
+white = (250, 250, 250)
+gray = (120, 120, 120)
+
+screen = pygame.display.set_mode(WIDTH, HEIGHT)
+pygame.display.set_caption('Beat Maker')
+
 
 
 
